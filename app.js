7425888ad20c7665779471033d155162812b0d5f
d0bl3dd <didier.agudo@cun.edu.co>
2026-05-09 14:38:01 -0500

    Fix Supabase real URL

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+console.log("APP NUEVA");
+
 const SUPABASE_URL =
-"https://bwvxyymzojuiuwmrjfsu.supabase.co";
+"https://bwvyxmyzojuiuwmrjfsu.supabase.co";
 
 const SUPABASE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dnl4bXl6b2p1aXV3bXJqZnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyOTQxNzksImV4cCI6MjA5Mzg3MDE3OX0.T1URlauIxhysbNY8wd86VEiAYN1v11qC4os2AD-ljZk";
+"sb_publishable_TkQbvH4IyYfXsVVAgFiFcQ_YtEHbQA4";
 
 const client = window.supabase.createClient(
     SUPABASE_URL,
@@ -25,8 +27,6 @@ btnLogin.addEventListener("click", async () => {
 
     mensaje.innerText = "";
 
-    // VALIDACIÓN
-
     if(!cuenta || !pin){
 
         mensaje.innerText =
@@ -35,41 +35,47 @@ btnLogin.addEventListener("click", async () => {
         return;
     }
 
-    // CONSULTA A SUPABASE
+    try{
 
-    const { data, error } = await client
-        .from("usuarios")
-        .select("*")
-        .eq("cuenta", cuenta)
-        .eq("pin", pin);
+        const { data, error } = await client
+            .from("usuarios")
+            .select("*")
+            .eq("cuenta", cuenta)
+            .eq("pin", pin);
 
-    // ERROR DE CONEXIÓN
-
-    if(error){
-
+        console.log(data);
         console.log(error);
 
-        mensaje.innerText =
-        "Error de conexión";
+        if(error){
 
-        return;
+            mensaje.innerText =
+            "Error de conexión";
+
+            return;
+        }
+
+        if(data.length > 0){
+
+            localStorage.setItem(
+                "usuario",
+                JSON.stringify(data[0])
+            );
+
+            window.location.href =
+            "dashboard.html";
+
+        }else{
+
+            mensaje.innerText =
+            "Cuenta o PIN incorrecto";
+        }
+
+    }catch(err){
+
+        console.log(err);
+
+        mensaje.innerText =
+        "Error inesperado";
     }
 
-    // LOGIN EXITOSO
-
-    if(data.length > 0){
-
-        localStorage.setItem(
-            "usuario",
-            JSON.stringify(data[0])
-        );
-
-        window.location.href =
-        "dashboard.html";
-
-    }else{
-
-        mensaje.innerText =
-        "Cuenta o PIN incorrecto";
-    }
 });
