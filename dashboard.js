82d5f3b233092e42a16032d5af5f07cc8495f0fe
d0bl3dd <didier.agudo@cun.edu.co>
2026-05-09 20:49:43 -0500

    dashboard update

--- dashboard.js
+++ dashboard.js
@@ -1,34 +1,138 @@
-const usuario = JSON.parse(
+const usuario =
+JSON.parse(
     localStorage.getItem("usuario")
 );
 
 if (!usuario) {
-    window.location.href = "index.html";
+
+    window.location.href =
+    "index.html";
 }
 
-const nombre = document.getElementById("nombre");
-const cuenta = document.getElementById("cuenta");
-const saldo = document.getElementById("saldo");
+const nombre =
+document.getElementById("nombre");
+
+const cuenta =
+document.getElementById("cuenta");
+
+const saldo =
+document.getElementById("saldo");
 
 if (nombre) {
+
     nombre.innerText =
-        usuario.nombre || "Usuario";
+    usuario.nombre || "Usuario";
 }
 
 if (cuenta) {
+
     cuenta.innerText =
-        usuario.cuenta || "----";
+    usuario.cuenta || "----";
 }
 
 if (saldo) {
+
     saldo.innerText =
-        "$ " + Number(usuario.saldo)
-        .toLocaleString();
+    "$ " +
+    Number(usuario.saldo)
+    .toLocaleString();
 }
 
-function logout() {
+function actualizarSaldo() {
 
-    localStorage.removeItem("usuario");
+    saldo.innerText =
+    "$ " +
+    Number(usuario.saldo)
+    .toLocaleString();
 
-    window.location.href = "index.html";
-} 
+    localStorage.setItem(
+        "usuario",
+        JSON.stringify(usuario)
+    );
+}
+
+function depositar() {
+
+    const input =
+    document.getElementById("deposito");
+
+    const monto =
+    Number(input.value);
+
+    if (
+        isNaN(monto) ||
+        monto <= 0
+    ) {
+
+        alert("Monto inválido");
+        return;
+    }
+
+    usuario.saldo += monto;
+
+    actualizarSaldo();
+
+    input.value = "";
+
+    alert(
+        "Depósito realizado correctamente"
+    );
+}
+
+function retirar() {
+
+    const input =
+    document.getElementById("retiro");
+
+    const monto =
+    Number(input.value);
+
+    if (
+        isNaN(monto) ||
+        monto <= 0
+    ) {
+
+        alert("Monto inválido");
+        return;
+    }
+
+    if (
+        monto > usuario.saldo
+    ) {
+
+        alert(
+            "Saldo insuficiente"
+        );
+
+        return;
+    }
+
+    usuario.saldo -= monto;
+
+    actualizarSaldo();
+
+    input.value = "";
+
+    alert(
+        "Retiro realizado correctamente"
+    );
+}
+
+const logoutBtn =
+document.getElementById("logout");
+
+if (logoutBtn) {
+
+    logoutBtn.addEventListener(
+        "click",
+        () => {
+
+            localStorage.removeItem(
+                "usuario"
+            );
+
+            window.location.href =
+            "index.html";
+        }
+    );
+}
